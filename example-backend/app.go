0ace73f427ac7c37b5d86913b2b68f48d912fc84
Jami Kousa <jami.kousa@helsinki.fi>
2021-02-07 17:05:29 +0200

    backend tests

--- example-backend/app.go
+++ example-backend/app.go
@@ -1,69 +1,13 @@
 package main
 
 import (
-	"net/http"
 	"os"
-	"server/cache"
 	"server/common"
-	"server/controller"
-	"server/pgconnection"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
+	"server/router"
 )
-
-func pingpong(context *gin.Context) {
-
-	redis := context.Query("redis") == "true"
-	postgres := context.Query("postgres") == "true"
-
-	if redis {
-		_, err := cache.TryRedis()
-		if err != nil {
-			context.String(http.StatusNotImplemented, err.Error())
-			return
-		}
-		context.String(http.StatusOK, "pong")
-		return
-	}
-	if postgres {
-		_, err := pgconnection.TryPostgres()
-		if err != nil {
-			context.String(http.StatusNotImplemented, err.Error())
-			return
-		}
-		context.String(http.StatusOK, "pong")
-		return
-	}
-
-	context.String(http.StatusOK, "pong")
-}
 
 // Start server
 func main() {
 	port := common.FallbackString(os.Getenv("PORT"), "8080")
-	allowedOrigin := common.FallbackString(os.Getenv("REQUEST_ORIGIN"), "https://example.com")
-
-	cache.InitializeRedisClient()
-	pgconnection.InitializePostgresClient()
-
-	config := cors.DefaultConfig()
-
-	config.AllowOrigins = []string{allowedOrigin}
-
-	router := gin.Default()
-
-	router.Use(cors.New(config))
-
-	router.GET("/ping", pingpong)
-
-	router.GET("/messages", controller.GetMessages)
-	router.POST("/messages", controller.CreateMessage)
-
-	router.NoRoute(func(context *gin.Context) {
-		path := context.Request.URL.Path
-		context.String(404, "The only API of this app is /ping. Request received to path "+path+" and this resulted in 404.")
-	})
-
-	router.Run(":" + port)
+	router.Router().Run(":" + port)
 }
