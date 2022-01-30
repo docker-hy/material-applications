341bd0289b05283278672e68521593af24fc5a1f
Jami Kousa <jami.kousa@helsinki.fi>
2022-01-30 17:52:16 +0200

    move to cra

--- scaling-exercise/calculator/src/index.js
+++ scaling-exercise/calculator/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { render } from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
+import ReactDOM from 'react-dom';
 import App from './components/App'
 
-
-const refresh = () => render(
-  <App />,
-  document.getElementById('root'),
-)
-
-refresh()
-
-if (module.hot) {
-  module.hot.accept()
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
