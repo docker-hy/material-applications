a86cf402639887145ed4c910188d854a7fcf4f7a
Larry Gadallah <larry@gadallah.com>
2024-05-19 16:20:48 -0700

    Revert "Adding emphasis to message"
    This reverts commit 5fb4b99b8911cf31867805708136af0c8c4f336b.

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 8080
